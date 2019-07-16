abfccd31f30e64941cde0d6ebcb6cb6331e17110
冯学仕 <335629146@qq.com>
2019-07-16 14:54:00 +0800

    addBezieiPathDrawtext

--- FSSwiftLink/Classes/extension/EUIView.swift
+++ FSSwiftLink/Classes/extension/EUIView.swift
@@ -102,24 +102,6 @@ extension UIView {
         self.layer.masksToBounds = false
     }
     
-    
-    //获取控制器
-    public func viewController()->UIViewController? {
-        
-        var nextResponder: UIResponder? = self
-        
-        repeat {
-            nextResponder = nextResponder?.next
-            
-            if let viewController = nextResponder as? UIViewController {
-                return viewController
-            }
-            
-        } while nextResponder != nil
-        
-        return nil
-    }
-    
 }
 
 // MARK: - 方法
