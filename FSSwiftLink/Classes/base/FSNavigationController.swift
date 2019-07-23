b16eca407a1a7fdb1d7c7fd4a26626d89459ae70
冯学仕 <335629146@qq.com>
2019-07-23 14:47:42 +0800

    public navigation base

--- FSSwiftLink/Classes/base/FSNavigationController.swift
+++ FSSwiftLink/Classes/base/FSNavigationController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FSNavigationController: UINavigationController {
+open class FSNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
